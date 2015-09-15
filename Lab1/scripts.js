3bf8eeb73aa36b79d2706e8b468bcd404d2ceb2c
Nayeema Lail <w0270771@nscc.ca>
2015-09-15 10:00:28 -0700

    Test to commit a directory

---
+++ Lab1/scripts.js
@@ -0,0 +1,13 @@
+	"use strict";
+	
+	function sayTwoPlusTwo(){
+		var total;
+		total = 2 + 2;
+		alert("Two plus two equals: " + total);
+	}
+		
+	function sayTwoPlusThree(){
+		var total;
+		total = 2 + 3;
+		alert("Two plus three equals: " + total);
+	}
