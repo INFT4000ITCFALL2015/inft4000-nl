9c3e69e5824ca98b9a7c6c74887245c3c3d9790c
Nayeema Lail <w0270771@nscc.ca>
2015-10-20 09:35:53 -0700

    Assignment 1 - Reset Button Implementation & Content Editable using a function Done

--- Assignment1/script.js
+++ Assignment1/script.js
@@ -1,24 +1,38 @@
 "use strict";
+window.document.body.onload = setEditableTrue;
+
 function isNumberKey(event) {
     var charCode = event.which || event.keyCode;
 
     if (charCode > 31 && (charCode < 49 || charCode > 57)) {
-        return false;
-    }
 
-    return true;
-}
-
-function validNumbers(field)
-{
-    if(/^[1-9]{1}$/.test(field.value))
-    {
         return true;
     }
-    else
-    {
-        alert("Not Text");
+    else {
+        alert ('Not string');
         return false;
     }
 }
+
+function setEditableTrue() {
+
+    var contentEditable = document.getElementsByClassName('editable');
+    for(var i=0; i < contentEditable.length; i++) {
+        contentEditable[i].setAttribute('contenteditable', true);
+    }
+}
+
+
+
+document.getElementById("btnReset").addEventListener("click",function() {
+
+    var originalEmptyCell = document.getElementsByClassName('originalEmptyCell');
+    for(var i=0; i<originalEmptyCell.length; i++) {
+           originalEmptyCell[i].innerHTML = "&nbsp;&nbsp;";
+    }
+});
+
+
+
+
 
