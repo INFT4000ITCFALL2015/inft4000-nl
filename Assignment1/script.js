05732f455bd43255da650f6dc5d8485a3b04ed7f
Nayeema Lail <w0270771@nscc.ca>
2015-10-21 08:44:26 -0700

    Assignment 1 - Optimized some code for the load and reset function and sudoku.htm

--- Assignment1/script.js
+++ Assignment1/script.js
@@ -1,38 +1,38 @@
 "use strict";
-window.document.body.onload = setEditableTrue;
+window.document.body.onload = setEditableTrueAndKeyup;
 
-function isNumberKey(event) {
-    var charCode = event.which || event.keyCode;
-
-    if (charCode > 31 && (charCode < 49 || charCode > 57)) {
-
-        return true;
-    }
-    else {
-        alert ('Not string');
-        return false;
-    }
+function setEditableTrueAndKeyup() {
+      var contentEditable = document.getElementsByClassName('editable');
+      for(var i=0; i < contentEditable.length; i++) {
+          contentEditable[i].setAttribute('contenteditable', true);
+          contentEditable[i].addEventListener('keyup', function(){
+              validNumberCheck(this.textContent)
+          });
+      }
 }
 
-function setEditableTrue() {
-
-    var contentEditable = document.getElementsByClassName('editable');
-    for(var i=0; i < contentEditable.length; i++) {
-        contentEditable[i].setAttribute('contenteditable', true);
-    }
-}
-
-
-
-document.getElementById("btnReset").addEventListener("click",function() {
-
-    var originalEmptyCell = document.getElementsByClassName('originalEmptyCell');
-    for(var i=0; i<originalEmptyCell.length; i++) {
-           originalEmptyCell[i].innerHTML = "&nbsp;&nbsp;";
-    }
+ document.getElementById("btnReset").addEventListener("click",function() {
+     var originalEmptyCell = document.getElementsByClassName('editable');
+     for(var i=0; i<originalEmptyCell.length; i++) {
+            originalEmptyCell[i].innerHTML = "&nbsp;&nbsp;";
+     }
 });
 
+function validNumberCheck(curObj){
 
+    var myRegExp = new RegExp(/^[1-9]$/);
 
+    var input = curObj.trim();
 
+    if(myRegExp.test(input))
+    {
+       return true;
+    }
+    else
+    {
+        alert('You need to enter number between 1-9');
 
+        return false;
+
+    }
+}
