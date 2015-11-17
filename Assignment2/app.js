abaa712d863eb665988df804f54f74b309a0c0eb
Nayeema Lail <w0270771@nscc.ca>
2015-11-17 09:59:29 -0800

    Assignment2 - Added the JSON Array when hover over the table

--- Assignment2/app.js
+++ Assignment2/app.js
@@ -4,6 +4,18 @@ app.controller('StudentController', function($scope, $http) {
     $scope.sortType     = 'name'; // set the default sort type
     $scope.sortReverse  = false;  // set the default sort order
     $scope.searchName   = '';     // set the default search/filter term
+    $scope.myStyle= {
+        "position": "absolute",
+        "right": "150px",
+        "top": "450px",
+        "z-index": "-1",
+        "width": "600px",
+        "height": "400px",
+        "border": "15px groove #8AC007"
+    }
+
+    $scope.showReport= false;
+    $scope.studentJSON = "";
 
     $http.get("students.json").success(function(response) {
         $scope.students = response.records;
@@ -35,6 +47,18 @@ app.controller('StudentController', function($scope, $http) {
         }//end if newStudentId
     };//addnewStudent function
 
+    $scope.displayJSONReport = function () {
+        //set the showReport to true
+        $scope.showReport= true;
+        //create a variable with the current json as string studentJSON
+        $scope.studentJSON = JSON.stringify($scope.students);
+
+    };//displayJSONReport function
+
+    $scope.hideReport=function(){
+        $scope.showReport= false;
+    };
+
     $scope.removeRow = function(studentId){
         var index = -1;
         var comArr = eval( $scope.students );
@@ -51,6 +75,7 @@ app.controller('StudentController', function($scope, $http) {
     };
 
 });
+
 
 app.filter('capitalize', function() {
     return function(input) {
@@ -83,3 +108,10 @@ app.directive('numbersOnly', [
         };
     }
 ])
+
+    .directive("studentDetails", function() {
+        return {
+            restrict: 'E',
+            templateUrl: "student-details.html"
+        };
+    });// end of directive code
