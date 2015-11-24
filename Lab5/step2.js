63235bd2d50fcbda5871a242735a02a6bb5278bb
Nayeema Lail <w0270771@nscc.ca>
2015-11-24 07:55:03 -0800

    Lab5 - Done from step 1-13 with package.json and excluded the node_module folders

--- Lab5/step2.js
+++ Lab5/step2.js
@@ -1,10 +1,7 @@
 /**
  * Created by inet2005 on 11/17/15.
  */
-var myArgs = process.argv.slice(2);
-total = 0;
-for(var i in myArgs)
-{
-    total += +(myArgs[i]);
-}
-console.log(total);
+var sum = 0;
+for( var i = 2; i < process.argv.length; i++ )
+    sum += Number( process.argv[ i ] );
+console.log( sum );
--- Lab5/step8.js
+++ Lab5/step8.js
@@ -1,10 +1,8 @@
 var http = require( 'http' );
 var bl = require( 'bl' );// collect buffers and access with a standard readable Buffer interface,
 http.get( process.argv[ 2 ], function( response ) {
-    response.pipe(
-        bl(function( err, data ) {
-                if ( err )
-                    return console.error( "Error: " + err );
+    response.pipe(bl(function( err, data ) {
+                if ( err ) return console.error( "Error: " + err );
                 data = data.toString();
                 console.log( data.length );
                 console.log( data );
