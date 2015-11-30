f8fc7f915b02bd14cd9e954ad04932895d054df1
Nayeema Lail <w0270771@nscc.ca>
2015-11-30 05:30:49 -0800

    Lab6 - REST operation done using Nodejs except create havnig some issues in inserting employee_id

---
+++ Lab6/controllers/employeeController.js
@@ -0,0 +1,135 @@
+/**
+ * Created by inet2005 on 11/26/15.
+ */
+module.exports.index = function(req, res) {
+
+    console.log("Inside of the module - index method");
+    req.getConnection(function(err, connection) {//getting the connection
+        if (err) return next(err);
+
+        var query = connection.query('SELECT * FROM employees ORDER BY emp_no DESC LIMIT 0,10', [], function(err, results) {
+            if (err){
+                return next(err);
+            }
+
+            res.json(results);
+
+        });
+        console.log(query.sql);
+    });
+
+};
+
+
+//module.exports.create =function(req, res) {
+//
+//    console.log("Inside of the module - creating method");
+//    var input = JSON.parse(JSON.stringify(req.body));
+//
+//    req.getConnection(function (err, connection) {
+//
+//        var query = connection.query('SELECT MAX(emp_no) as Employee_id FROM employees',  function(err, results) {
+//            if (err){
+//                return next(err);
+//            }
+//
+//            //console.log(results[0]);
+//
+//
+//            var data = {
+//                emp_no: input.results['Employee_id'],
+//                first_name: input.first_name,
+//                last_name: input.last_name,
+//                birth_date: input.birth_date,
+//                hire_date: input.hire_date,
+//                gender: input.gender
+//            };
+//
+//            var query = connection.query("INSERT INTO employees set ? ", data, function (err, rows) {
+//
+//                if (err) {
+//                    console.log("Error inserting : %s ", err);
+//                }
+//                else {
+//                    res.json({message: 'employee created!'});
+//                }
+//
+//            });
+//        });
+//        console.log(query.sql);
+//
+//    });
+//
+//};
+
+
+module.exports.show = function(req, res) {//getting the connection
+    console.log("Inside of the module - show method");
+    req.getConnection(function(err, connection) {//getting the connection
+        if (err) return next(err);
+
+        var query = connection.query('SELECT * FROM employees WHERE emp_no = ? ', req.params.emp_no, function(err, results) {
+            if (err){
+                return next(err);
+            }
+
+            res.json(results);
+        });
+        console.log(query.sql);
+    });
+
+};
+
+module.exports.update = function(req, res) {
+    console.log("Inside of the module - update method");
+    var input = JSON.parse(JSON.stringify(req.body));
+    var emp_no = req.params.emp_no;
+
+    req.getConnection(function (err, connection) {
+
+        var data = {
+
+            first_name    : input.first_name,
+            last_name : input.last_name
+        };
+
+
+        var query = connection.query("UPDATE employees set ? WHERE emp_no = ? ",[data,emp_no], function(err, rows)
+        {
+
+            if (err) {
+                console.log("Error Updating : %s ", err);
+            }
+            else{
+                res.json({message: 'Employee updated!' });
+            }
+
+        });
+
+        console.log(query.sql);
+
+    });
+};
+
+module.exports.destroy = function(req, res) {
+    var emp_no = req.params.emp_no;
+
+    req.getConnection(function (err, connection) {
+
+        var query = connection.query("DELETE FROM employees  WHERE emp_no = ? ",[emp_no], function(err, rows)
+        {
+
+            if (err) {
+                console.log("Error Deleting : %s ", err);
+            }
+            else{
+                res.json({message: 'Employee deleted!' });
+            }
+
+        });
+
+        console.log(query.sql);
+
+    });
+
+};
--- Lab6/routes/index.js
+++ Lab6/routes/index.js
@@ -1,11 +1,23 @@
 var express = require('express');
 var router = express.Router();
-
+var employeeController = require('../controllers/employeeController');
 // get an instance of the express Router
+
+// middleware to use for all requests
+router.use(function(req, res, next) {
+    // do logging
+    console.log('Something is happening.');
+
+    //allows cross-origin resource sharing
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "X-Requested-With");
+    next(); // make sure we go to the next routes and don't stop here
+});
+
 
 // test route to make sure everything is working (accessed at GET http://localhost:8080/api)
 router.get('/', function(req, res) {
-  res.json({ message: 'hooray! welcome to our api!' });
+    res.json({ message: 'hooray! welcome to our api!' });
 });
 
 /* GET home page. */
@@ -17,29 +29,18 @@ router.get('/', function(req, res) {
 
 router.route('/employees')
 
-  // create an actor (accessed at POST http://localhost:3000/api/actors)
-    .post(function(req, res) {
+    // create an employee (accessed at POST http://localhost:3000/api/employees)
+    //.post(employeeController.create)
 
-      //
+    // get all the employees (accessed at GET http://localhost:3000/api/employees)index
+    .get(employeeController.index)
 
-    })
+    router.route('/employees/:emp_no')
 
-  // get all the actors (accessed at GET http://localhost:3000/api/actors)
-    .get(function(req, res) {
-
-      req.getConnection(function(err, connection) {
-        if (err) return next(err);
-
-        connection.query('SELECT * FROM employees LIMIT 0,10', [], function(err, results) {
-          if (err){
-            return next(err);
-          }
-
-          res.json(results);
-
-        });
-      });
-
-    });
+// get the employee with that id (accessed at GET http://localhost:3000/api/employees/:emp_no)
+    .get(employeeController.show)
+    .put(employeeController.update)
+    // delete the employee with this id (accessed at DELETE http://localhost:8080/api/employees/:emp_no)
+   .delete(employeeController.destroy);
 
 module.exports = router;
